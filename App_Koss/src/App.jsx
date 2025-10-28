cda37ae52ed0f6d426e1774e5367f8c5bd8810ee
khs1115 <asd7480asd@gmail.com>
2025-10-28 18:18:54 +0900

    [FEAT]5,6,7,8,9 과제 반영버전

--- App_Koss/src/App.jsx
+++ App_Koss/src/App.jsx
@@ -1,6 +1,7 @@
 /*delint-disable*/
+import React from 'react'
 
-import { useState } from 'react';
+import { use, useState } from 'react';
 import './App.css'
 
 function App(){
@@ -9,21 +10,44 @@ function App(){
     let [글제목,글제목변경] = useState(['남자코트추천','강남 우동 맛집','파이썬 독학'])
     let [좋아요,좋아요변경] = useState([0 ,0 ,0]);
     let [modal, setModal] = useState(0);
+    let [title, setTitle] = useState(0);
+    let [입력값, 입력값변경] = useState('');
 
-
-    function 함수(){
-      console.log(1);
-    }
+    function change(){
+                let copy = [...글제목];
+                copy[0] = '여자코트추천';
+                글제목변경(copy);
+            }
     
-    function Modal(){
+    function Modal(props){
         return(
             <div className="modal">
-                <h4>제목</h4>
+                <h4>{props.작명[props.title]}</h4>
                 <p>날짜</p>
                 <p>상세내용</p>
+                <button onClick={props.change}>글수정</button>
             </div>
         )
     }
+
+    // class Modal2 extends React.Component {
+    //     constructor(props){
+    //         super(props)
+    //         this.state = {
+    //             name : 'kim',
+    //             age : 20
+    //         }
+    //     }
+    //     render(){
+    //         return(
+    //             <div>안녕 {this.state.age}
+    //             <button onClick={() => {
+    //                 this.setState({age : 21})
+    //             }}>버튼</button>
+    //             </div>
+    //         )
+    //     }
+    // }
 
     return(
         <div className="App">
@@ -36,11 +60,7 @@ function App(){
                 글제목변경(sort_abc);
             }}>가나다순 정렬</button>
 
-            <button onClick={ () => {
-                let copy = [...글제목];
-                copy[0] = '여자코트추천';
-                글제목변경(copy);
-            }}>글 수정</button>
+            <button onClick={change}>글 수정</button>
 
             {/* <div className='list'>
                 <h4>{글제목[0]} <span onClick={() =>{ 좋아요변경(좋아요+1) }}>😝</span> {좋아요} </h4>
@@ -57,22 +77,50 @@ function App(){
 
             {
                 글제목.map(function(제목,i){
-                    return <div className='list'>
-                    <h4 onClick={() => {modal == 0 ? setModal(1) : setModal(0) }}>{제목} </h4> <span onClick={() =>{ 
+                    return <div className="list" key={i}>
+                    <h4 onClick={(e) => { e.stopPropagation();
+                        modal == 1 && title == i ? setModal(0) : setModal(1) 
+                        setTitle(i)
+                    }}> {제목}
+                    <span onClick={(e) =>{ e.stopPropagation();
                     let 좋아요_copy = [...좋아요];
                     좋아요_copy[i] += 1 
-                    좋아요변경(좋아요_copy);}}>😝</span> {좋아요[i]}
+                    좋아요변경(좋아요_copy);}}>😝</span> {좋아요[i]} 
+                    <button  className="del" onClick={(e) =>{ 
+                        e.stopPropagation();
+                        let copy_1 = [...글제목];
+                        copy_1.splice(i,1)
+                        글제목변경(copy_1)
+                        let copy_2 = [...좋아요];
+                        copy_2.splice(i,1)
+                        좋아요변경(copy_2)
+                    } }
+                    >삭제</button> </h4> 
                     <p>2월 17일 발행</p>
+                    
                 </div>
                 })
             }
 
             
-            {
-                modal == 0 ? null : <Modal/>
+            <input onChange={(e) => { 입력값변경(e.target.value)
+           
+            }}></input>
+
+            <button onClick={() => {
+                let copy_1 = [...글제목];
+                copy_1.unshift(입력값)
+                글제목변경(copy_1)
+                let copy_2 = [...좋아요];
+                copy_2.unshift(0)
+                좋아요변경(copy_2)
             }
+            }>글 추가</button>
 
-
+            {
+                 modal == 0 ? null : <Modal title={title} change={change} 작명={글제목}/>
+            }
+            {/* <Modal2></Modal2> */}
         </div>
     );
 }
